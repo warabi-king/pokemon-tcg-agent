00d68c4626ecc3e724983f965ccd10687be131dc
m0mizuki <mutou.m.2020@gmail.com>
2026-07-02 21:37:00 +0900

    feat: エージェントをルールベース(ルカリオex)に変更

--- src/cg/sim.py
+++ src/cg/sim.py
@@ -1,6 +1,5 @@
 import ctypes
 import os
-import platform
     
 class StartData(ctypes.Structure):
     _fields_ = [
@@ -17,13 +16,8 @@ class SerialData(ctypes.Structure):
         ("selectPlayer", ctypes.c_int)
     ]
 
-os_name = platform.system()
-if os_name == 'Windows':
+if os.name == 'nt':
     lib_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "cg.dll")
-elif os_name == "Darwin":
-    lib_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "libcg.dylib")
-elif platform.machine() in ('arm64', 'aarch64'):
-    lib_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "libcg-arm64.so")
 else:
     lib_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "libcg.so")
 lib = ctypes.cdll.LoadLibrary(lib_path)
